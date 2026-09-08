57bc80a3d8f2fdf2b3c0962768c383caf50c6e70
Avika <avika.jainn18@gmail.com>
2026-09-08 10:07:19 +0530

    add helper functions

--- Lab3/App.js
+++ Lab3/App.js
@@ -1,9 +1,42 @@
 import http from "http";
+import { getAllTeams } from "./teams.js";
+import { resolve } from "dns";
 
+const PORT = 5000
+
+const sendJson = (res, statusCode, data) => {
+    res.writeHead(statusCode, { "content-type": "application/json" });
+    res.end(data === "undefined" ? "" : JSON.stringify(data));
+};
+const parseJSONBody = (req) => {
+    new Promise((resolve,reject)=> {
+    let body = "";
+    req.on("data", (chunk) => {
+        body += chunk.toString();
+    });
+    req.on("end", () => {
+        try {
+            resolve(bosy ? JSON.parse(body) : {});
+        }
+        catch (error) {
+            reject(error);
+        }
+    });
+ });
+};
 const server = http.createServer((req, res) => {
-    res.end("<h1> SIH Internal </h1>");
+    if (req.url === '/' && req.method == 'GET') {
+        const teams = getAllTeams();
+        console.log("Teams: ", teams);
+        
+        res.write(JSON.stringify(teams));
+    }
+    else {
+        res.statusCode = 404;
+    }
+        res.end();
 });
 
-server.listen(5000, () => {
-    console.log("Sever is running");
+server.listen(PORT, () => {
+    console.log("SIH Sever is running at ", PORT);
 })
