163fbb384c493fc9b33d2f99003703a5bb73a41b
Avika <avika.jainn18@gmail.com>
2026-09-15 10:14:33 +0530

    complete-registration

--- lab4/teams.js
+++ lab4/teams.js
@@ -24,7 +24,7 @@ export const getTeamById = (id) => teams.find((team) => team.id === id);
 export const addTeam = (newTeam) => {
   const team = { id: nextId++, ...newTeam };
   teams.push(team);
-  return team;
+  return team; 
 };
 
 export const updateTeamById = (id, updateTeam) => {
