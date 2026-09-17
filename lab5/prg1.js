8d0869547765d9e84290a26735615feaeebddd53
Avika <avika.jainn18@gmail.com>
2026-09-17 14:37:42 +0530

    sending HTML data to client

---
+++ lab5/prg1.js
@@ -0,0 +1,23 @@
+import express from "express";
+import path from 'path';
+import { fileURLToPath } from "node:url";
+
+const PORT = 3333;
+const app = express();
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(dirname,'public','index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(dirname,'public','about.html'))
+})
+
+app.get('/enquiry', (req, res) => {
+    res.sendFile(path.join(dirname,'public','enquiry.html'))
+})
+
+app.listen(PORT, () => console.log("prg1 is running at 3333"));
