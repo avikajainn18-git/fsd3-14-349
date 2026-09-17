c748638d56ad8282a104cf82ff0e428c8c1b9166
Avika <avika.jainn18@gmail.com>
2026-09-17 14:51:54 +0530

    use express.static to set frontend

---
+++ lab5/prg2.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from 'path';
+import { fileURLToPath } from "node:url";
+
+const PORT = 3333;
+const app = express();
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+app.use(express.static(path.join(dirname, 'frontend')));
+app.use((req, res) => {
+    res.status(404).send("Resource not found");
+});
+app.listen(PORT, () => console.log("prg2 is running at 3333"));
